a4a8af2294f53b49071dc42e16e8451ae7099e6a
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-09 01:22:11 +1200

    Create Armour.java

---
+++ GUI_AdeventureGame/src/newCoolGame/Armour.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newCoolGame;
+
+/**
+ *
+ * @author Bradley
+ */
+public class Armour extends Node{
+    public Armour(int id, String name, int stat, int price) {
+        super(id, name, stat, price);
+    }
+    
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
