8a1ca920663f70bdcb53694a8f9454534294c33a
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-09 01:22:36 +1200

    Create Inventory.java

---
+++ GUI_AdeventureGame/src/newCoolGame/Inventory.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newCoolGame;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bradley
+ */
+public class Inventory {
+    private Weapon currentWeapon;
+    private Armour currentArmour;
+    
+   
+    
+    public void setCurrentWeapon(Weapon weapon) {
+        currentWeapon = weapon;
+    }
+    public void setCurrenArmour(Armour armour) {
+        currentArmour = armour;
+    }
+}
