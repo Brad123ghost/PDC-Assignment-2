471884059b65a4254ecd7dbe3ffe2d80eaf058b2
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-08 21:21:15 +1200

    Create MenuState.java

---
+++ GUI_AdeventureGame/src/Menu/MenuState.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Menu;
+
+/**
+ *
+ * @author Bradley
+ */
+public enum MenuState {
+    MAIN_MENU,
+    NEW_GAME,
+    LOAD_GAME,
+    EXIT_GAME
+}
