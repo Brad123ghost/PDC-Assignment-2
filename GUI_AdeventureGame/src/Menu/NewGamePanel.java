b3fcbaa408d7fd8c5562b21fa4ea8afe33dc4798
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-08 23:45:03 +1200

    Added an error message field

--- GUI_AdeventureGame/src/Menu/NewGamePanel.java
+++ GUI_AdeventureGame/src/Menu/NewGamePanel.java
@@ -5,7 +5,9 @@
 package Menu;
 
 import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 /**
@@ -14,28 +16,43 @@ import javax.swing.JTextField;
  */
 public class NewGamePanel extends MenuTemplate{
     private JTextField userNameField;
-    private JButton submitNameBtn;
+    private JButton startGameBtn;
+    private JLabel errorMsg;
     
     public NewGamePanel() {
         super.setTitleText("Enter Your Name");
         this.setLayout(null);
+        errorMsg = new JLabel("Invalid Input!");
+        errorMsg.setBounds(this.width/2-50, 260, 101, 30);
+        errorMsg.setForeground(Color.red);
+        errorMsg.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        errorMsg.setVisible(false);
+        this.add(errorMsg);
+        
         userNameField = new JTextField();
+//        userNameField.setLocation(this.width/2, 150);
+        userNameField.setBounds(this.width/2-125, 300, 250, 30);
+        userNameField.setBackground(Color.white);
         this.add(userNameField);
         
-        submitNameBtn = new JButton("Start");
-        submitNameBtn.setBounds(this.width/2-75, 350, 150, 50); 
-        submitNameBtn.setContentAreaFilled(true);
-        submitNameBtn.setFocusPainted(false);
-        submitNameBtn.setOpaque(true);
-        submitNameBtn.setBackground(Color.orange);
-        this.add(submitNameBtn);
+        startGameBtn = new JButton("Start");
+        startGameBtn.setBounds(this.width/2-75, 350, 150, 50); 
+        startGameBtn.setContentAreaFilled(true);
+        startGameBtn.setFocusPainted(false);
+        startGameBtn.setOpaque(true);
+        startGameBtn.setBackground(Color.orange);
+        this.add(startGameBtn);
     }
     
     public JTextField getNameField() {
         return this.userNameField;
     }
     
-    public JButton submitNameBtn() {
-        return this.submitNameBtn;
+    public JButton getStartBtn() {
+        return this.startGameBtn;
+    }
+    
+    public void setErrorMsg() {
+        this.errorMsg.setVisible(true);
     }
 }
