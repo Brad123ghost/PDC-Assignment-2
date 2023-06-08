2c5bc0c5429d0b6f5ed456358f6fd20d8f1d0104
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-08 18:14:48 +1200

    Update GameFrame.java

--- GUI_AdeventureGame/src/newCoolGame/GameFrame.java
+++ GUI_AdeventureGame/src/newCoolGame/GameFrame.java
@@ -4,10 +4,34 @@
  */
 package newCoolGame;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Bradley
  */
-public class GameFrame{
+public class GameFrame extends JFrame{
+    private JFrame frame;
     
+    private Menu menuModel;
+    private MenuPanel menuPanel;
+//    private JPanel menuJPanel;
+    
+    int width = 960;
+    int height = 540;
+    
+    public GameFrame() {
+        this.frame = new JFrame("Adventure Game");
+        this.frame.setSize(width, height);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.menuPanel = new MenuPanel();
+        this.menuModel = new Menu(menuPanel);
+        
+//        this.menuJPanel = menuPanel.getPanel();
+        
+        this.frame.add(menuPanel);
+        this.frame.setVisible(true);
+    }
 }
