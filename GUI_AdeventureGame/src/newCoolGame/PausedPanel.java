646510c9b79fa678c4ed8737d13fc566841b9080
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-09 20:42:19 +1200

    Update PausedPanel.java

--- GUI_AdeventureGame/src/newCoolGame/PausedPanel.java
+++ GUI_AdeventureGame/src/newCoolGame/PausedPanel.java
@@ -4,10 +4,41 @@
  */
 package newCoolGame;
 
+import java.awt.Color;
+import javax.swing.JButton;
+
 /**
  *
  * @author Bradley
  */
 public class PausedPanel extends MenuTemplate{
+    private JButton resumeGameBtn;
+    private JButton saveExitBtn;
     
+    public PausedPanel() {
+        super.setTitleText("Game Paused");
+        this.setLayout(null);
+        resumeGameBtn = new JButton("Resume Game");
+        resumeGameBtn.setBounds(this.width/2-75, 200, 150, 50);
+        resumeGameBtn.setContentAreaFilled(true);
+        resumeGameBtn.setFocusPainted(false);
+        resumeGameBtn.setBackground(Color.orange);
+        this.add(resumeGameBtn);
+        
+        saveExitBtn = new JButton("Save & Exit");
+        saveExitBtn.setBounds(this.width/2-75, 275, 150, 50);
+        saveExitBtn.setContentAreaFilled(true);
+        saveExitBtn.setFocusPainted(false);
+        saveExitBtn.setBackground(Color.orange);
+        this.add(saveExitBtn);
+
+    }
+    
+    public JButton getRGBtn() {
+        return this.resumeGameBtn;
+    }
+    
+    public JButton getSEBtn() {
+        return this.saveExitBtn;
+    }
 }
