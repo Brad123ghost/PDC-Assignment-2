a58c331496b1fae721927e8a46e08a60c9658d44
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-09 16:58:58 +1200

    Update Player.java

--- GUI_AdeventureGame/src/newCoolGame/Player.java
+++ GUI_AdeventureGame/src/newCoolGame/Player.java
@@ -33,8 +33,33 @@ public class Player {
         return playerInstance;
     }
     
-    public void setName(String name) {
-        this.name = name;
+    public double getHealth() {
+        return this.health;
+    }
+    public void setHealth(double health) {
+        this.health = health;
+        double rounded = this.health;
+        rounded = Math.round(rounded * 100);
+        rounded = rounded/100;
+        this.health = rounded;
+    }
+    
+    public String getProgress() {
+        return this.progress;
+    }
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+    
+    public int getCoins() {
+        return this.coins;
+    }
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+    
+    public void upgradeHealth() {
+        this.health += 5;
     }
  
 }
