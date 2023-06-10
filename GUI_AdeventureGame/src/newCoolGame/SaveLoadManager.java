150d121c427743918cd4990107bca21e3088648d
Bradley Chung <rcn6795@autuni.ac.nz>
2023-06-10 16:08:27 +1200

    Update SaveLoadManager.java

--- GUI_AdeventureGame/src/newCoolGame/SaveLoadManager.java
+++ GUI_AdeventureGame/src/newCoolGame/SaveLoadManager.java
@@ -26,14 +26,18 @@ public class SaveLoadManager {
     public void savePlayerData() {
         Player player = gMem.player;
         int playerID = player.playerID;
-        String name = player.name;
+        String name = player.name.toLowerCase();
         String progressID = player.getProgress();
         double health = player.getHealth();
         int coins = player.getCoins();
         int weaponID = player.inventory.getCurrentWeapon().getID();
         int armourID = player.inventory.getCurrentArmour().getID();
         
-        String insertQuery = "INSERT INTO PLAYER VALUES ("
+        String insertQuery = "";
+        String updateQuery = "";
+        System.out.println(searchPlayer(name));
+        if(!searchPlayer(name)) {
+            insertQuery = "INSERT INTO PLAYER VALUES ("
                 + playerID + ",'"
                 + name + "','"
                 + progressID + "',"
@@ -41,20 +45,64 @@ public class SaveLoadManager {
                 + coins + ","
                 + weaponID + ","
                 + armourID +")";
-        
-        dbManager.updateDB(insertQuery);
+            dbManager.updateDB(insertQuery);
+            System.out.println("Player Saved");
+        } else {
+            updateQuery = "UPDATE PLAYER SET"
+                    + "PROGRESSID = '" + progressID + "',"
+                    + "HEALTH = " + health + ","
+                    + "COINS = " + coins + ","
+                    + "WEAPONID = " + weaponID + ","
+                    + "ARMOURID = " + armourID
+                    + "WHERE PLAYERID = " + playerID; 
+            dbManager.updateDB(updateQuery);
+            System.out.println("Player Updated");
+        }
+
         System.out.println("Saved");
     }
     
     public void getPlayerList() {
         String selectQuery = "SELECT Name FROM PLAYER";   
-         try {
+        try {
             ResultSet rs = dbManager.queryDB(selectQuery);
-            if(rs.next()) {
+            while(rs.next()) {
                 gMem.userList.add(rs.getString("Name"));
             }  
         } catch (SQLException ex) {
             Logger.getLogger(SaveLoadManager.class.getName()).log(Level.SEVERE, null, ex);
         } 
     }
+    
+    public void retrievePlayerData(String name) {
+        String selectQuery = "SELECT * FROM PLAYER WHERE NAME = '" + name +"'"; 
+        try {
+            ResultSet rs = dbManager.queryDB(selectQuery);
+            if(rs.next()) {
+                gMem.player.playerID = rs.getInt("PlayerID");
+                gMem.player.name = rs.getString("Name");
+                gMem.player.progress = rs.getString("progressID");
+                gMem.player.health = rs.getDouble("Health");
+                gMem.player.coins = rs.getInt("Coins");
+                gMem.player.inventory.setCurrentWeapon((Weapon)gMem.shop.getWeapIndex(rs.getInt("WeaponID")));
+                gMem.player.inventory.setCurrentArmour((Armour)gMem.shop.getArmourIndex(rs.getInt("ArmourID")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SaveLoadManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+    
+    public boolean searchPlayer(String name) {
+        String selectQuery = "SELECT * FROM PLAYER WHERE NAME = '" + name +"'";
+        ResultSet rs = dbManager.queryDB(selectQuery);
+        try {
+            if(rs.next()) {
+                return true;
+            } 
+        } catch (SQLException ex) {
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+    
 }
